c4f5a089f60eea7650b1b87d3d0cf5eff16a5ed9
sly <slydevwebproject@outlook.fr>
2022-08-18 15:34:40 +0200

    review code & style

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 'use strict';
 
+let justCalculated = false;
+let lastCalc = '';
+
 function displayOnScreen(event) {
     let equalBtn = document.querySelector(".keyboard__key_equal");
     let acBtn = document.querySelector(".keyboard__key_ac");
     let nope = document.querySelector(".keyboard");
     let currentBtn = event.target;
-    let numb = currentBtn.innerText;
+    let numb = currentBtn.innerText.length === 1 ? currentBtn.innerText : '';
     let onScreen = document.querySelector(".display_screen");
+
     if (currentBtn != nope && currentBtn != acBtn && currentBtn != equalBtn) {
-        if (onScreen.innerHTML == "0"){
+        if (justCalculated) {
+            onScreen.innerText = "";
+            justCalculated = false;
+        }
+        if (onScreen.innerHTML == "0") {
             onScreen.innerHTML = "";
         }
         onScreen.innerHTML += numb;
@@ -17,17 +25,28 @@ function displayOnScreen(event) {
 
 function cancel(event) {
     let onScreen = document.querySelector(".display_screen");
-    let currentBtn = event.target;
+    lastCalc = '';
     onScreen.innerHTML = "0";
 }
 
 function calculate() {
-    let calc = document.querySelector(".display");
-    console.log(eval(calc.innerText));
     let onScreen = document.querySelector(".display_screen");
-    onScreen.innerHTML = eval(calc.innerText);
-    console.log(onScreen.innerHTML);
+    let calc = document.querySelector(".display");
+    let theResult = '';
+    try {
+        calc.innerText[0]?.match(/[-*+/]/)?
+            theResult = eval(lastCalc + calc.innerText)
+            :
+            theResult = eval(calc.innerText);
+    } catch (error) {
+        console.log(error);
+        theResult = '3RR0R';
+    }
+    onScreen.innerHTML = theResult;
+    justCalculated = true;
+    lastCalc = theResult;
 }
+
 
 let myKeyboard = document.querySelector(".keyboard");
 myKeyboard.addEventListener("click", displayOnScreen);
